f12d2a38b013cc03636dcfd2ffe057e21d9b93e6
Egor <voronkow.egor@yandex.ru>
2022-04-01 20:43:52 +0300

    ДЗ номер 2

---
+++ hw02_studentTest/src/main/java/ru/otus/domain/Answer.java
@@ -0,0 +1,11 @@
+package ru.otus.domain;
+
+public class Answer {
+    String text;
+    boolean correct;
+
+    public Answer(String text, boolean correct) {
+        this.text = text;
+        this.correct = correct;
+    }
+}
