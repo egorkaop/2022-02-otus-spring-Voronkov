30caba056e83045a747c75dbc408ce93caa8adf4
Egor <voronkow.egor@yandex.ru>
2022-06-08 22:46:05 +0300

    Добавил удаление

--- hw10_WebLibrary/src/main/java/ru/otus/controller/BookControllerRest.java
+++ hw10_WebLibrary/src/main/java/ru/otus/controller/BookControllerRest.java
@@ -1,7 +1,9 @@
 package ru.otus.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.dto.BookDto;
 import ru.otus.service.BookService;
@@ -18,5 +20,11 @@ public class BookControllerRest {
         List<BookDto> bookDtoList = bookService.getAllBooks();
         System.out.println(bookDtoList.get(0).getTitle());
         return bookService.getAllBooks();
+    }
+
+    @DeleteMapping("/api/books/{id}")
+    public void deleteBook(@PathVariable long id){
+        System.out.println(id);
+        bookService.deleteBookById(id);
     }
 }
